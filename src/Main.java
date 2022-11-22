b3e733accac0e2c5fee1c90040f989eef8cdca53
Pablo Aguiar Raposo <pabloaguiar92@hotmail.com>
2022-11-22 10:04:12 -0300

    tratando div por 0

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Olá! Neste programa você digitará números e depois calcularei a média aritmética ou harmônica entre eles");
+
+            System.out.println("Olá! Neste programa você digitará números e depois calcularei a média aritmética ou harmônica entre eles.");
             System.out.println("Primeiro, digite a quantidade de números que serão usados no cálculo");
             Scanner entrada = new Scanner(System.in);
             int quantidade = pegaNumInt();
+            while (quantidade < 2) {
+                System.out.println("por favor, passe um número maior ou igual a 2");
+                quantidade = pegaNumInt();
+            }
             double[] lista = new double[quantidade];
 
             for (int i = 0; i < quantidade; i++) {
@@ -19,13 +24,11 @@ public class Main {
             System.out.println("para média harmônica, digite: HARMONICA");
             String media = entrada.nextLine();
 
-
             double soma = 0;
             int nElementos = lista.length;
 
             boolean sair = false;
             while(!sair) {
-
                 if (Objects.equals(media, "ARITMETICA")) {
                     System.out.print("Números digitados: ");
 
@@ -33,12 +36,18 @@ public class Main {
                         soma += lista[i];
                         System.out.print(lista[i] + " ");
                     }
-                    System.out.println("");
+                    System.out.println();
                     System.out.println("A média escolhida foi a aritmética!");
                     System.out.println("Média: " + (soma / nElementos));
                     sair = true;
 
                 } else if (Objects.equals(media, "HARMONICA")) {
+                    for (int i = 0; i < quantidade; i++) {
+                        if(lista[i] == 0) {
+                            System.out.println("A média não pode ser calculada, pois não é possível dividir um número por 0.");
+                            System.exit(0);
+                        }
+                    }
                     System.out.print("Números digitados: ");
 
                     for (int i = 0; i < quantidade; i++) {
@@ -49,11 +58,15 @@ public class Main {
                     for (int i = 0; i < quantidade; i++) {
                         lista[i] = 1 / lista[i];
                         somaInversa += lista[i];
-                        sair = true;
                     }
-                    System.out.println("");
-                    System.out.println("A média escolhida foi a harmônica!");
-                    System.out.println("Média: " + (nElementos / somaInversa));
+                        if (somaInversa == 0) {
+                            System.out.println("A média não pode ser calculada, pois não é possível dividir um número por 0.");
+                        } else {
+                            System.out.println();
+                            System.out.println("A média escolhida foi a harmônica!");
+                            System.out.println("Média: " + (nElementos / somaInversa));
+                        }
+                    sair = true;
                 }
                 else {
                     System.out.println("Valor inválido!");
